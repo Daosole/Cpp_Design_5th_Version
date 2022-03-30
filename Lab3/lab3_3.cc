793c35044dcb9a55a4fdbbf27211f48287baf1f8
Daosole <keivong@outlook.com>
2022-03-30 08:51:39 +0800

    update: add lab3_3

---
+++ Lab3/lab3_3.cc
@@ -0,0 +1,15 @@
+// 系统函数 求 pow(x,y)
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(void)
+{
+    long long x, y, re;
+    cout << "input x and y to calculate:" << endl;
+    cin >> x >> y;
+    re = pow(x, y);
+
+    cout << "The result is:" << re << endl;
+
+    return 0;    
+}
