99d34a7ba2fac0dd1b7574ead5740839f4345772
Daosole <keivong@outlook.com>
2022-04-01 09:58:28 +0800

    update: 函数重载举例

---
+++ Example3/3_16.cc
@@ -0,0 +1,28 @@
+// 重载函数
+#include <iostream>
+using namespace std;
+
+int sumOfSquare(int a, int b)
+{
+    return a * a + b * b;
+}
+
+double sumOfSquare(double a, double b)
+{
+    return a * a + b * b;
+}
+
+int main(void)
+{
+    int m, n;
+    cout << "Enter two integers: ";
+    cin >> m >> n;
+    cout << "Their sum of square: " << sumOfSquare(m, n) << endl;
+
+    double x, y;
+    cout << "Enter two real numbers: ";
+    cin >> x >> y;
+    cout << "Their sum of square: " << sumOfSquare(x, y) << endl;
+
+    return 0;
+}
