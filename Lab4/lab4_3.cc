0577dc7e671d72c6d75d53cb52a3e425e9ae1832
Daosole <keivong@outlook.com>
2022-04-30 13:24:39 +0800

    update: add lab4_3

---
+++ Lab4/lab4_3.cc
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+
+class Date
+{
+  private:
+    int year;
+    int month;
+    int day;
+
+  public:
+    Date() : year(0), month(0), day(0)
+    {
+    }
+    Date(int y, int m, int d) : year(y), month(m), day(d)
+    {
+    }
+    void setBrith();
+    int getY()
+    {
+        return year;
+    };
+    int getM()
+    {
+        return month;
+    };
+    int getD()
+    {
+        return day;
+    };
+    ~Date()
+    {
+    }
+};
+
+void Date::setBrith()
+{
+    int yy, mm, dd;
+    cout << "Enter the birthday:" << endl;
+    cin >> yy >> mm >> dd;
+    year = yy;
+    month = mm;
+    day = dd;
+}
+
+class People
+{
+  private:
+    int number;
+    char sex;
+    Date birthday;
+    long Id;
+
+  public:
+    People(int num, char s, Date bir, long id) : number(num), sex(s), birthday(bir), Id(id)
+    {
+    }
+    People(People &p);
+    void show();
+    void setInfo(int num, char s, Date bir, long id);
+    ~People()
+    {
+    }
+};
+
+People::People(People &p)
+{
+    number = p.number;
+    sex = p.sex;
+    birthday = p.birthday;
+    Id = p.Id;
+}
+
+void People::show()
+{
+    cout << "The identification" << endl;
+    cout << "\tNumber:" << number << endl;
+    cout << "\tsex:" << sex << endl;
+    cout << "\tBirthday:" << birthday.getY() << birthday.getM() << birthday.getD() << endl;
+    cout << "\tId:" << Id << endl;
+}
+
+void People::setInfo(int num, char s, Date bir, long id)
+{
+    number = num;
+    sex = s;
+    birthday = bir;
+    Id = id;
+}
+
+int main(void)
+{
+    Date birth(1996,05,23);
+    Date b2;
+    b2.setBrith();
+    People a(4001, 'F', birth, 100110119120);
+    People b = a;
+    a.show();
+    b.show();
+    a.setInfo(4003, 'M', b2, 10001001029);
+    a.show();
+
+    return 0;
+
+}
