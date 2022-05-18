e4b5daa7f663900ea3db74a801ba7b02534c134a
Daosole <keivong@outlook.com>
2022-05-18 09:16:41 +0800

    feat: 实现 3*3 转置，不通用

--- Example6/trans_row.cc
+++ Example6/trans_row.cc
@@ -1,16 +1,16 @@
 #include <iostream>
+
 using namespace std;
 
-int main(void)
-{
+int main(void) {
     void trans(int(*pointer)[3]);
-    int a[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int a[3][3] = {{1, 2, 3},
+                   {4, 5, 6},
+                   {7, 8, 9}};
 
     cout << "The original array:" << endl;
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             cout << "\t" << a[i][j];
         }
         cout << endl;
@@ -30,13 +30,15 @@ int main(void)
     return 0;
 }
 
-void trans(int(*pointer[3]))
-{
+void trans(int(*pointer)[3]) {
     int temp;
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = i; j < 3; j++)
-        {
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = i; j < 3; j++) {
+            temp = *(pointer[i] + j);
+            *(pointer[i] + j) = *(pointer[j] + i);
+            *(pointer[j] + i) = temp;
+//            cout << *(pointer[i] + j);
         }
     }
 }
