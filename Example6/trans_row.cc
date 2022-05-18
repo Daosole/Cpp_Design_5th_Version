15b4482f87748b901bd78ce7cd6b9c0a04699efd
Daosole <keivong@outlook.com>
2022-05-18 09:41:07 +0800

    feat: 更加通用的转置
    使用数组指针:tada:

--- Example6/trans_row.cc
+++ Example6/trans_row.cc
@@ -1,16 +1,21 @@
 #include <iostream>
-
+#define R 4 // row 行
+#define C 3 // column 列
 using namespace std;
 
-int main(void) {
+int main(void)
+{
     void trans(int(*pointer)[3]);
-    int a[3][3] = {{1, 2, 3},
+    int a[R][C] = {{1, 2, 3},
                    {4, 5, 6},
-                   {7, 8, 9}};
+                   {7, 8, 9},
+                   {10, 11, 12}};
 
     cout << "The original array:" << endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
             cout << "\t" << a[i][j];
         }
         cout << endl;
@@ -18,27 +23,36 @@ int main(void) {
     cout << "After changed:" << endl;
     trans(a);
 
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cout << "\t" << a[i][j];
-        }
-        cout << endl;
-    }
+    //    for (int i = 0; i < R; i++)
+    //    {
+    //        for (int j = 0; j < C; j++)
+    //        {
+    //            cout << "\t" << a[i][j];
+    //        }
+    //        cout << endl;
+    //    }
 
     return 0;
 }
 
-void trans(int(*pointer)[3]) {
-    int temp;
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = i; j < 3; j++) {
-            temp = *(pointer[i] + j);
-            *(pointer[i] + j) = *(pointer[j] + i);
-            *(pointer[j] + i) = temp;
-//            cout << *(pointer[i] + j);
+void trans(int (*pointer)[3])
+{
+    int temp[C][R];
+    for (int i = 0; i < C; i++)
+    {
+        for (int j = 0; j < R; j++)
+        {
+            temp[i][j] = *(pointer[j] + i);
         }
+    }
+
+    cout << "temp array:" << endl;
+    for (int i = 0; i < C; i++)
+    {
+        for (int j = 0; j < R; j++)
+        {
+            cout << "\t" << temp[i][j];
+        }
+        cout << endl;
     }
 }
