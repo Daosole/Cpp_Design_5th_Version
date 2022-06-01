aacd6dacfef8f94ebc88189e13195ee33b9a17f2
Daosole <keivong@outlook.com>
2022-06-01 09:47:20 +0800

    update: add lab6-3

---
+++ Lab6/lab6_3.cc
@@ -0,0 +1,28 @@
+//字符串连接
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    char s1[100], s2[50];
+    cout << "Enter two strings: " << endl;
+    cout << "String1: ";
+    cin >> s1;
+    cout << "String2: ";
+    cin >> s2;
+    int i = 0, j = 0;
+    while (s2[j] != '\0')
+    {
+        j++;
+    }
+    i = j;
+    for (j = 0; s2[j] != '\0'; j++)
+    {
+        s1[i++] = s2[j];
+    }
+    s1[i] = '\0';
+
+    cout << "After Linked" << endl;
+    cout << s1;
+    return 0;
+}
