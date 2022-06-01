a74e2d7bce93f2b9426142c3d9b3cf8d5629cd5b
Daosole <keivong@outlook.com>
2022-06-01 09:40:55 +0800

    update: add lab6-4
    :tada:

---
+++ Lab6/lab6_4.cc
@@ -0,0 +1,20 @@
+// 使用 string 类
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    string s1, s2;
+    cout << "Enter two strings: " << endl;
+    cout << "String1: ";
+    getline(cin, s1);
+    cout << "String2: ";
+    getline(cin, s2);
+
+    s1 += s2;
+
+    cout << "After Linked: " << endl;
+    cout << s1;
+    return 0;
+}
