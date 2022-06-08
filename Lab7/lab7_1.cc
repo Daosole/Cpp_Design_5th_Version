f58214c123d59d2f1b7b2f5112ba99a960b7d3fa
Daosole <keivong@outlook.com>
2022-06-08 08:43:02 +0800

    update: lab 7-1
    wrong code just to learn about Derived class

---
+++ Lab7/lab7_1.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+private:
+    int age;
+};
+
+class dog : public Animal
+{
+public:
+    void setAge(int n);
+    void show()
+    {
+        cout << "Age:" << age << endl;
+    }
+};
+
+void dog::setAge(int n)
+{
+    age = n;
+}
+
+int main(void)
+{
+    dog d;
+    d.setAge(3);
+    d.show();
+
+    return 0;
+}
