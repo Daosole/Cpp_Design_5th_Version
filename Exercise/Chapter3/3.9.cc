8ec3fcd6065895fa84610213812e50fb5bb9279b
Daosole <keivong@outlook.com>
2022-06-12 13:34:48 +0800

    update: add 3.9
    质数

---
+++ Exercise/Chapter3/3.9.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int prime(int i);
+
+int main()
+{
+    int i;
+    cout << "Enter a number: ";
+    cin >> i;
+    if (prime(i))
+        cout << i << " is a prime." << endl;
+    else
+        cout << i << " is not a prime."  << endl;
+    return 0;
+}
+
+int prime(int i)
+{
+    int j, k, flag;
+    flag = 1;
+    k = sqrt(i);
+    for (j = 2; j <= k; j++)
+    {
+        if (i % j == 0)
+        {
+            flag = 0;
+            break;
+        }
+
+    }
+    return flag;
+}
