187a54824bf2d67a9ccea746ff16cfd243213f6e
Daosole <keivong@outlook.com>
2022-06-12 13:29:10 +0800

    update: add 3.7 3.8

---
+++ Exercise/Chapter3/3.8.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    float Convert(float x);
+    float cel, fah;
+    cout << "Please input Fahrenheit:" << endl;
+    cin >> fah;
+    cel = Convert(fah);
+    cout << "The Celsius is:" << " " << cel << endl;
+
+    return 0;
+}
+
+float Convert(float x)
+{
+    float z;
+    return z = (x - 32) * 5.0 / 9;
+}
